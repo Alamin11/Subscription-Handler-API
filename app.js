71bee8fd8e246c51b65adf4546ce7bdb4b211177
Alamin11 <alamincsecu@gmail.com>
2025-02-13 17:44:51 +0600

    First commit with all the setup and section like config, routes, models and global error handler

---
+++ app.js
@@ -0,0 +1,35 @@
+// console.log("server is running on port 5000");
+
+import express from "express";
+
+import { PORT } from "./config/env.js";
+
+import authRouter from "./routes/auth.routes.js";
+import userRouter from "./routes/user.routes.js";
+import subscriptionRouter from "./routes/subscription.routes.js";
+import connectDB from "./database/mongodb.js";
+import errorMiddleware from "./middleware/error.middleware.js";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/subscriptions", subscriptionRouter);
+
+app.use(errorMiddleware);
+
+app.get("/", (req, res) => {
+  res.send("Server is ready");
+});
+
+app.listen(PORT, () => {
+  console.log(`server is running on port: http://localhost:${PORT}`);
+  connectDB();
+});
+
+export default app;
